b431c51fa92b1acd35f58a2e61c16bfdc865de53
Nathan <42022943+GitSiracusa@users.noreply.github.com>
2018-10-11 15:31:31 +1100

    initial

---
+++ MyFirstWebApp/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Contact")]
+    public class ContactController : Controller
+    {
+        [HttpPost]
+        public IActionResult Post(string name, string email, string message)
+        {
+            return Ok("Yoooooooooo");
+        }
+    }
+}
